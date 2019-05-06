bf1f608ce0716ec54fa7fc25c4fe582d71c931d2
Jack Hawkins <jack.hawkins77@googlemail.com>
2019-05-06 22:26:16 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -45,11 +45,6 @@ epic_tragedy = {
         :age => "30",
         :attitude => "confused"
       },
-      {
-        :name => "Nurse",
-        :age => "44",
-        :attitude => "worried"
+      {:name => "Nurse", :age => "44", :attitude => "worried"}]
       }
-    ]
-  }
 }
